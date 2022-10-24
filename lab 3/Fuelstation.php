19494e4803a3a7220ad6ce96d2a6d72d3a050675
Sofia <93678956+SofiaMudrenok@users.noreply.github.com>
2022-10-24 23:32:27 +0300

    lab3

--- lab 3/Fuelstation.php
+++ lab 3/Fuelstation.php
@@ -7,4 +7,22 @@ class Fuelstation
     public $owner;
     public $fuel;
     public $price;
+    public function __construct($code, $array)
+    {
+        $this->code = $code;
+        $this->address = $array['address'];
+        $this->owner = $array['owner'];
+        $this->fuel = $array['fuel'];
+        $this->price = $array['price'];
+    }
+    public static function validationData($array){
+        return !(
+            empty($array['address']) ||
+            empty($array['owner']) ||
+            empty($array['fuel']) ||
+            empty($array['price']) ||
+            !isset($array)
+        );
+    }
+
 }
--- lab 3/index.php
+++ lab 3/index.php
@@ -1,180 +1,91 @@
 <h1>Palivo.ua</h1>
 <?php
-function getSimpleData() {
-    return [
-        [
-            "code" => 1,
-            "address" => "Kapushanska",
-            "owner" => "WOG",
-            "fuel" => 500,
-            "price" => 41
-        ],
-        [
-            "code" => 2,
-            "address" => "Minayska",
-            "owner" => "WOG",
-            "fuel" => 600,
-            "price" => 41
-        ],
-        [
-            "code" => 3,
-            "address" => "Sobranetska",
-            "owner" => "OKKO",
-            "fuel" => 550,
-            "price" => 40
-        ],
-        [
-            "code" => 4,
-            "address" => "Minayska",
-            "owner" => "OKKO",
-            "fuel" => 400,
-            "price" => 40
-        ],
-        [
-            "code" => 5,
-            "address" => "Timiryazeva",
-            "owner" => "WOG",
-            "fuel" => 300,
-            "price" => 41
-        ],
-    ];
+
+include "Fuelstation.php";
+include "FuelstationColection.php";
+
+if (!isset($_SESSION)) {
+    session_start();
 }
 
-function getUniqueCode(array $fuelstations, int $proposedCode) {
-    if (count($fuelstations) == 0) {
-        return $proposedCode;
+if (empty($_SESSION['Fuelstations'])) {
+    $_SESSION['Fuelstations'] = new FuelstationColection();
+    $_SESSION['Fuelstations']->defaultFuelstation();
+}
+
+$actionToDo = $_POST['action'];
+echo $_SESSION['Fuelstations']->showFuelstations();
+if ($actionToDo == 'add') {
+    if (Fuelstation::validationData($_POST)) {
+        $_SESSION['Fuelstations']->addFuelstation(
+            new Fuelstation(5, $_POST)
+        );
     }
-    $max = $fuelstations[0]['code'];
-    foreach ($fuelstations as $fuelstation) {
-        if ($fuelstation['code'] > $max) {
-            $max = $fuelstation['code'];
-        }
+} elseif ($actionToDo == 'edit') {
+    if (Fuelstation::validationData($_POST)) {
+        $_SESSION['Fuelstations']->editFuelstation(
+            $_POST
+        );
     }
-    if ($proposedCode > $max) {
-        return $proposedCode;
-    }
-    $max++;
-    return $max;
-}
-function sortByFuelQuantity(int $x, string $owner, $arr) {
-    $newArr = [];
-    for ($i = 0; $i < count($arr); $i++) {
-        if ($owner == $arr[$i]["owner"] && $arr[$i]["fuel"] >= $x) {
-            array_push($newArr, $arr[$i]);
-        }
-    }
-    return $newArr;
-}
-
-function fillData($factories, $data) {
-    return [
-        "code" => getUniqueCode($factories, $data['code']),
-        "address" => $data["address"],
-        "owner" => $data["owner"],
-        "fuel" => $data["fuel"],
-        "price" => $data["price"]
-    ];
-}
-function validateFactoryData($data):bool {
-    if (empty($data["code"])
-        || empty($data["address"])
-        || empty($data["owner"])
-        || empty($data["fuel"])
-        || empty($data["price"])) {
-        return false;
-    }
-    return true;
-}
-
-session_start();
-
-$fourthPoint = $_GET['fourthPoint'];
-if (isset($_SESSION['fuelstation'])) {
-    $fuelstation = $_SESSION['fuelstation'];
-} else {
-    $fuelstation = getSimpleData();
-}
-
-if (!empty($_GET["edit"])) {
-    if (validateFactoryData($_GET)) {
-        for ($i = 0; $i < count($fuelstation); $i++) {
-            if ($_GET["edit"] == $fuelstation[$i]["code"]) {
-                $fuelstation[$i] = fillData($fuelstation, $_GET);
-                break;
-            }
-        }
-    } else {
-        //TODO: Error handling
-    }
-} elseif (array_key_exists('code', $_GET)) {
-    if (validateFactoryData($_GET)) {
-        $fuelstation[] = fillData($fuelstation, $_GET);
-    } else {
-        //TODO: Error handling
-    }
+} elseif ($actionToDo == 'filter') {
+    echo $_SESSION['Fuelstations']->filter($_POST['owner'], $_POST['fuel']);
+} elseif ($actionToDo == 'save') {
+    $_SESSION['Fuelstations']->save();
+} elseif ($actionToDo == 'load') {
+    $_SESSION['Fuelstations']->load();
 }
 
 
-$_SESSION["fuelstation"] = $fuelstation;
-
-echo "<h2>Таблиця всіх АЗС</h2>";
-echo "<table border='1px'>";
-echo "<tr> <th>Code</th> <th>Address</th> <th>Owner</th> <th>Fuel</th> <th>Price</th> </tr>";
-for($i = 0; $i < count($fuelstation); $i++){
-    echo "<tr>";
-    foreach ($fuelstation[$i] as $key=> $value){
-        if ($value != null) {
-            echo "<td>$value</td>";
-        }
-    }
-    echo "</tr>";
-}
-echo "</table>";
-
-
-$arr = sortByFuelQuantity(500,"WOG",$fuelstation);
-echo "<h2>Запит  наявності X літрів пального</h2>";
-echo "<span>X = 500</span><br><span>АЗС: WOG</span>";
-echo "<table border='1px'>";
-echo "<tr> <th>Code</th> <th>Address</th> <th>Owner</th> <th>Fuel</th> <th>Price</th> </tr>";
-for ($i = 0; $i < count($arr); $i++) {
-    echo "<tr>";
-    foreach ($arr[$i] as $key => $value) {
-        echo "<td>$value</td>";
-    }
-    echo "</tr>";
-}
-echo "</table>";
-if(isset($_POST["save"])){
-    $file = fopen("fuel.txt", "w");
-    fwrite($file, serialize($_SESSION["fuelstation"]));
-    fclose($file);
-}
-elseif(isset($_POST["load"])) {
-    $_SESSION['fuelstation'] = unserialize(file_get_contents("fuel.txt"));
-}
 
 ?>
-
-<form method="get" action="">
-    <input type="number" name="edit" placeholder="Pick Code"> <br>
-    <input type="number" name="code" placeholder="Code"> <br>
-    <input type="text"  name="address" placeholder="Address"> <br>
-    <input type="text"  name="owner" placeholder="Owner"> <br>
-    <input type="number" name="fuel" placeholder="Fuel "> <br>
-    <input type="number" name="price"  placeholder="Price"> <br>
-    <input type="submit" name="btn-ok" value="ok">
-
-    <input type="hidden" name="fourthPoint" value="">
+<br>
+<form action='<?= $_SERVER['PHP_SELF'] ?>' method='post' id='filterForm'>
+    <label> Owner:
+        <input type='text' name='owner'>
+    </label><br>
+    <label> Fuel quantity:
+        <input type='number' name='fuel'>
+    </label><br>
+    <input type='hidden' name='action' value='filter'>
+    <input type='submit' value='Filter'>
 </form>
-<form action="<?php $_SERVER['PHP_SELF'];?>" method="post" id="save">
-    <input type="hidden" name="save" value="save">
-    <input type="submit" value='Save to file'>
+<form action='<?= $_SERVER['PHP_SELF'] ?>' method='post' id='addForm'>
+    <h2>ADD</h2>
+    <label> code:
+        <input type='number' name='code'>
+    </label><br>
+    <label> address:
+        <input type='text' name='address'>
+    </label><br>
+    <label> owner:
+        <input type='text' name='owner'>
+    </label><br>
+    <label> fuel:
+        <input type='number' name='fuel'>
+    </label><br>
+    <label> price:
+        <input type='number' name='price'>
+    </label><br>
+    <input type='hidden' name='action' value='add'>
+    <input type='submit' value='add'>
 </form>
-
-<form action="<?php $_SERVER['PHP_SELF'];?>" method="post" id='load'>
-    <input type='hidden' name='action' value='load'>
-    <input type='submit' value='Upload from file'>
+<form action='<?= $_SERVER['PHP_SELF'] ?>' method='post' id='editForm'>
+   <h2>EDIT</h2>
+    <label> code:
+        <input type='number' name='code'>
+    </label><br>
+    <label> address:
+        <input type='text' name='address'>
+    </label><br>
+    <label> owner:
+        <input type='text' name='owner'>
+    </label><br>
+    <label> fuel:
+        <input type='number' name='fuel'>
+    </label><br>
+    <label> price:
+        <input type='number' name='price'>
+    </label><br>
+    <input type='hidden' name='action' value='edit'>
+    <input type='submit' value='edit'>
 </form>
-
-</div>
